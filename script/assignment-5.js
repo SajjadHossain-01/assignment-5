a97ea5a11a910a2a678d6097fcce04300432eb96
sajjad Hossain <bslsajjad01@gmail.com>
2025-01-13 23:20:23 +0600

    do responsive

--- script/assignment-5.js
+++ script/assignment-5.js
@@ -5,6 +5,7 @@ function bookingSeat(ElementId) {
 
     // console.log(seatsSelected)
     if (seatsSelected.length <= 3) {
+        showTotalBookinSeat(ElementId)
         getBackgroundColor.classList.add('bg-lime-500');
         seatsSelected.push(selectedSeat);
         const arrayLenght = seatsSelected.length;
@@ -19,33 +20,52 @@ function bookingSeat(ElementId) {
         const showPrice = totalPrice('selected-seat');
         setElementById('tolat-price', showPrice);
 
-    
+
     }
-    if (seatsSelected.length >= 2 ) {
+    if (seatsSelected.length >= 2) {
         const cupponButtonId = document.getElementById('cuppon-input');
-        
+
         cupponButtonId.removeAttribute("disabled");
     }
 }
 
-function showDiscuntPrice(){
+function showDiscuntPrice() {
     const cupponInput = getInputValueById('discund-by-cuppon');
-    if(cupponInput === 'NEW15'){
-        const getTotalPrice =getElementById('tolat-price');
+    if (cupponInput === 'NEW15') {
+        const getTotalPrice = getElementById('tolat-price');
         const getTotalPriceNumber = Number(getTotalPrice);
-        const discundPrice = getTotalPriceNumber - getTotalPriceNumber * 15/100 ;
+        const discundPrice = getTotalPriceNumber - getTotalPriceNumber * 15 / 100;
         setElementById('grand-total', discundPrice);
-        console.log(getTotalPriceNumber)   
+        console.log(getTotalPriceNumber)
     }
-    else if(cupponInput === 'Couple 20' ){
-        const getTotalPrice =getElementById('tolat-price');
+    else if (cupponInput === 'Couple 20') {
+        const getTotalPrice = getElementById('tolat-price');
         const getTotalPriceNumber = Number(getTotalPrice);
-        const discundPrice = getTotalPriceNumber - getTotalPriceNumber * 20/100 ;
+        const discundPrice = getTotalPriceNumber - getTotalPriceNumber * 20 / 100;
         setElementById('grand-total', discundPrice);
     }
     // else {alert('thikmoto cuppon den vai')}
 }
 
-function showSelectedTiket() {
+function showTotalBookinSeat(ElementId) {
+    const getBackgroundColor = document.getElementById(ElementId);
+    const selectedSeat = getBackgroundColor.innerText;
+
+    const displaySelectedTiket = document.getElementById('show-selected-seat-infu');
+    const div = document.createElement('div');
+    const h1 = document.createElement('h1');
+    const h2 = document.createElement('h1');
+    const h3 = document.createElement('h1');
+
+    div.classList = "flex justify-between text-gray-500";
+
+    h1.innerText = selectedSeat;
+    h2.innerText = 'Economy';
+    h3.innerText = '550';
+    div.appendChild(h1);
+    div.appendChild(h2)
+    div.appendChild(h3)
+    displaySelectedTiket.appendChild(div);
+
 
 }
